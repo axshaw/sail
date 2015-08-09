ee303d5eba32d8cc8b18f40afb37bd0ec45cd3fc
alex <alex@icarix.net>
2015-08-09 23:46:10 +0100

    inital commit

---
+++ weather.py
@@ -0,0 +1,11 @@
+import json, requests
+
+# Get a webpage, this creates a Response object called "r"
+
+url = 'http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/310120?res=3hourly&key=cee0b1ec-a751-458a-894e-6aec68a91beb'
+
+
+resp = requests.get(url=url)
+data = json.loads(resp.text)
+
+print (data)
